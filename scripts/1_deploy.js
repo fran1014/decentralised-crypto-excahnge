f7047491ba30e72dcc744bf1e74ce8ab7bc0d2f6
Franklin <franklin.op@hotmail.com>
2024-01-01 16:49:20 +0100

    contract deployed displayed in console

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,16 +1,15 @@
-import { ethers } from 'hardhat';
-
+// Define the main function as an asynchronous function
 async function main() {
-  //Fetch contract to deploy
-
+  // Fetch contract to deploy
   const Token = await ethers.getContractFactory('Token');
 
-  //Deploy contract
+  // Deploy contract
   const token = await Token.deploy();
   await token.deployed();
   console.log(`Token Deployed to: ${token.address}`);
 }
 
+// Call the main function
 main()
   .then(() => process.exit(0))
   .catch((error) => {
