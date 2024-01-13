9c2f1176da182a2c5d51b7ab88688df9619b7ac4
Franklin <franklin.op@hotmail.com>
2024-01-13 14:18:46 +0100

    implement all functions from token transfers and testing

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -13,7 +13,12 @@ contract Token{
 
     //Track Balances 
     mapping(address => uint256) public balanceOf;
-    //Send tokens 
+    
+    event Transfer(
+    address indexed from,
+    address indexed to,
+    uint256 value
+      );
 
     constructor(
         string memory _name,
@@ -26,11 +31,21 @@ contract Token{
         balanceOf [msg.sender] = totalSupply;
     }
     
-    function transfer(address _to, uint256 _value) public 
-    returns (bool success){
+    function transfer(address _to, uint256 _value) 
+        public 
+        returns (bool success){
+        //Require that sender has enough tokens to spend
+        require(balanceOf[msg.sender] >= _value);
+        require(_to != address(0))
+
         //Deducts token from spender 
-    balanceOf[msg.sender] =  balanceOf[msg.sender] - _value;
+        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
         //Credit tokens to receiver
-    balanceOf[_to]  = balanceOf[_to] + _value;
+        balanceOf[_to] = balanceOf[_to] + _value;
+
+        //Emit event
+        emit Transfer(msg.sender, _to, _value);
+
+        return true;
     }
 }
