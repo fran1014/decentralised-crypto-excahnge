c757ac78553d65f29e3a6e78a6ed702bece51dbb
Franklin <franklin.op@hotmail.com>
2024-01-14 14:18:18 +0100

    finalize creating erc20 token before startint to create the exchange

--- contracts/Token.sol
+++ contracts/Token.sol
@@ -43,17 +43,24 @@ contract Token{
         returns (bool success){
         //Require that sender has enough tokens to spend
         require(balanceOf[msg.sender] >= _value);
-        require(_to != address(0));
 
-        //Deducts token from spender 
-        balanceOf[msg.sender] = balanceOf[msg.sender] - _value;
-        //Credit tokens to receiver
-        balanceOf[_to] = balanceOf[_to] + _value;
-
-        //Emit event
-        emit Transfer(msg.sender, _to, _value);
+        _transfer(msg.sender, _to, _value);
 
         return true;
+    }
+
+    function _transfer(
+      address _from,
+      address _to,
+      uint256 _value
+    )internal{
+      require(_to != address(0));
+      balanceOf[_from] = balanceOf[_from] - _value;
+        //Credit tokens to receiver
+      balanceOf[_to] = balanceOf[_to] + _value;
+
+      emit Transfer(_from, _to, _value);
+
     }
 
     function approve (address _spender, uint256 _value) 
@@ -61,10 +68,26 @@ contract Token{
     returns (bool success)
     {
        require(_spender != address(0));
-       
+
        allowance[msg.sender][_spender] = _value;
 
        emit Approval(msg.sender, _spender, _value);
        return true;
+    }
+
+    function transferFrom (address _from, address _to, uint256 _value ) 
+    public 
+    returns (bool success)
+    {
+
+    require(_value <= balanceOf[_from]);
+    require(_value <= allowance[_from][msg.sender]);
+    
+     //Reset allowance 
+     allowance[_from][msg.sender] = allowance[_from][msg.sender] - _value;
+
+     //spend tokens
+     _transfer(_from, _to, _value);
+     return true;
     }
 }
--- test/Token.js
+++ test/Token.js
@@ -131,5 +131,65 @@ describe('Token', () => {
         ).to.be.reverted;
       });
     });
+
+    describe('Delegated Token Transfers', () => {
+      let amount, transaction, result;
+
+      beforeEach(async () => {
+        amount = tokens(100);
+        transaction = await token
+          .connect(deployer)
+          .approve(exchange.address, amount);
+        result = await transaction.wait();
+      });
+      describe('Success', () => {
+        beforeEach(async () => {
+          transaction = await token
+            .connect(exchange)
+            .transferFrom(deployer.address, receiver.address, amount);
+          result = await transaction.wait();
+        });
+
+        it('transfers token balances', async () => {
+          expect(await token.balanceOf(deployer.address)).to.be.equal(
+            ethers.utils.parseUnits('999900', 'ether')
+          );
+          expect(await token.balanceOf(receiver.address)).to.be.equal(amount);
+        });
+
+        it('resets the allowance', async () => {
+          expect(
+            await token.allowance(deployer.address, exchange.address)
+          ).to.be.equal(0);
+        });
+
+        it('emits a Transfer event', async () => {
+          const event = result.events[0];
+          expect(event.event).to.equal('Transfer');
+
+          const args = event.args;
+          expect(args.from).to.equal(deployer.address);
+          expect(args.to).to.equal(receiver.address);
+          expect(args.value).to.equal(amount);
+        });
+      });
+      describe('Failure', () => {
+        it('rejects insufficient balances', async () => {
+          const invalidAmount = tokens(100000000);
+          await expect(
+            token.connect(deployer).transfer(receiver.address, invalidAmount)
+          ).to.be.reverted;
+        });
+
+        it('rejects invalid recipent', async () => {
+          const amount = tokens(100);
+          await expect(
+            token
+              .connect(deployer)
+              .transfer('0x0000000000000000000000000000000000000000', amount)
+          ).to.be.reverted;
+        });
+      });
+    });
   });
 });
