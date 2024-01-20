957029a62c015fe88a3b77252fe081a332cf40eb
Franklin <franklin.op@hotmail.com>
2024-01-20 10:44:52 +0100

    deploying tokens to blockchain

--- scripts/1_deploy.js
+++ scripts/1_deploy.js
@@ -1,12 +1,34 @@
 // Define the main function as an asynchronous function
 async function main() {
+  console.log(`Preparing deployment...\n`);
+
   // Fetch contract to deploy
   const Token = await ethers.getContractFactory('Token');
+  const Exchange = await ethers.getContractFactory('Exchange');
+
+  //Fetch accounts
+  const accounts = await ethers.getSigners();
+
+  console.log(
+    `Accounts fetched:\n${accounts[0].address}\n${accounts[1].address}\n`
+  );
 
   // Deploy contract
-  const token = await Token.deploy();
+  const token = await Token.deploy('My Token', 'Token', '1000000');
   await token.deployed();
   console.log(`Token Deployed to: ${token.address}`);
+
+  const mETH = await Token.deploy('mETH', 'mETH', '1000000');
+  await mETH.deployed();
+  console.log(`mETH Deployed to: ${mETH.address}`);
+
+  const mDAI = await Token.deploy('mDAI', 'mDAI', '1000000');
+  await mDAI.deployed();
+  console.log(`mDAI Deployed to:${mDAI.address}`);
+
+  const exchange = await Exchange.deploy(accounts[1].address, 10);
+  await exchange.deployed();
+  console.log(`Exchange Deployed to: ${exchange.address}`);
 }
 
 // Call the main function
