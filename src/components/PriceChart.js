c2a2fce24d22b90a185400ccd367049dc1ebfd00
Franklin <franklin.op@hotmail.com>
2024-02-04 15:10:00 +0100

    update price chart config and pricechart

--- src/components/PriceChart.js
+++ src/components/PriceChart.js
@@ -1,21 +1,34 @@
 import { useSelector } from 'react-redux';
 import Chart from 'react-apexcharts';
-import { options, series } from './PriceChart.config';
+
+import arrowDown from '../assets/down-arrow.svg';
+import arrowUp from '../assets/up-arrow.svg';
+import { options, defaulSeries } from './PriceChart.config';
+
+import { priceChartSelector } from '../store/selectors';
 import Banner from './Banner';
 
 const PriceChart = () => {
   const account = useSelector((state) => state.provider.account);
   const symbols = useSelector((state) => state.tokens.symbols);
+  const priceChart = useSelector(priceChartSelector);
   return (
     <div className="component exchange__chart">
       <div className="component__header flex-between">
         <div className="flex">
           <h2> {symbols && `${symbols[0]}/${symbols[1]}`} </h2>
 
-          <div className="flex">
-            <img src="" alt="Arrow down" />
-            <span className="up"></span>
-          </div>
+          {priceChart && (
+            <div className="flex">
+              {priceChart.lastPriceChange === '+' ? (
+                <img src={arrowUp} alt="Arrow up" />
+              ) : (
+                <img src={arrowDown} alt="Arrow down" />
+              )}
+
+              <span className="up"> {priceChart.lastPrice} </span>
+            </div>
+          )}
         </div>
       </div>
 
@@ -26,7 +39,7 @@ const PriceChart = () => {
         <Chart
           type="candlestick"
           options={options}
-          series={series}
+          series={priceChart ? priceChart.series : defaulSeries}
           width="100%"
           height="100%"
         />
