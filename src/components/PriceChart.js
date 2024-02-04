40d64a1be9a9242de04fa542060aee4a63d9ba49
Franklin <franklin.op@hotmail.com>
2024-02-04 02:15:09 +0100

    created PriceChar and banner component

---
+++ src/components/PriceChart.js
@@ -0,0 +1,29 @@
+import { useSelector } from 'react-redux';
+import Banner from './Banner';
+
+const PriceChart = () => {
+  const account = useSelector((state) => state.provider.account);
+  return (
+    <div className="component exchange__chart">
+      <div className="component__header flex-between">
+        <div className="flex">
+          <h2></h2>
+
+          <div className="flex">
+            <img src="" alt="Arrow down" />
+            <span className="up"></span>
+          </div>
+        </div>
+      </div>
+
+      {/* Price chart goes here */}
+      {!account ? (
+        <Banner text={'Please connect with Metamask'} />
+      ) : (
+        <p>Price chart placeholder</p>
+      )}
+    </div>
+  );
+};
+
+export default PriceChart;
